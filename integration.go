ca6823a9fd4f9726f53174727d05f9f06ec95e5a
Jide Tireni <jtirenipraise@gmail.com>
2025-02-18 21:28:00 +0100

    added app logo

--- integration.go
+++ integration.go
@@ -14,29 +14,29 @@ func getIntegrationJSON(c *gin.Context) {
 				"updated_at": "2025-02-18",
 			},
 			"descriptions": map[string]string{
-				"app_name":         "Grafana-Loki Integration",
 				"app_description":  "Fetches logs from a Loki server and sends them to a Telex channel at defined intervals.",
-				"app_logo":         "https://miro.medium.com/v2/resize:fit:1400/1*k-hdOAQjRXKoyguzKuoeKg.png",
+				"app_logo":         "https://grafana.com/media/docs/loki/logo-grafana-loki.png",
+				"app_name":         "Grafana-Loki Integration",
 				"app_url":          "https://telex-integration-production.up.railway.app",
 				"background_color": "#fff",
 			},
 			"integration_category": "Monitoring & Logging",
 			"is_active":            true,
 			"integration_type":     "interval",
-			"author":               "Tireni",
 			"key_features": []string{
 				"Fetch logs from Loki at regular intervals",
 				"Filter logs based on custom Loki queries",
 				"Send logs to a designated Telex channel",
 				"Monitor specific applications or services",
 			},
+			"author": "Tireni",
 			"settings": []map[string]interface{}{
 				{"label": "Loki Server URL", "type": "text", "required": true, "default": "http://localhost:3100"},
 				{"label": "Loki Query", "type": "text", "required": true, "default": "{job='varlogs'}"},
 				{"label": "Interval", "type": "text", "required": true, "default": "*/5 * * * *", "description": "Cron expression defining how often logs are fetched"},
 			},
 			"tick_url":   "https://telex-integration-production.up.railway.app/tick",
-			"target_url": "",
+			"target_url": "https://ping.telex.im/v1/return",
 		},
 	}
 
