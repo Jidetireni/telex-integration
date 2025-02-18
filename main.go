28034f816cad61d9cee638eb89591070096f8db1
Jide Tireni <jtirenipraise@gmail.com>
2025-02-18 23:19:40 +0100

    added cors

--- main.go
+++ main.go
@@ -3,11 +3,17 @@ package main
 import (
 	"telex-integration/loki"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
+	r.Use(cors.New(cors.Config{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{"GET", "POST"},
+		AllowHeaders: []string{"Content-Type", "Authorization"},
+	}))
 	r.GET("/integration.json", getIntegrationJSON)
 	r.POST("/tick", loki.TickHandler)
 	// r.GET("/logs-endpoint", loki.LogsEndpointHandler)
